b8afffd9687e29cc7a8d7dff9983b6eb5354734d
Motheo <motheotb@gmail.com>
2022-11-09 18:23:41 +0200

    Kotlin Learning

---
+++ src/generics/Aquarium.kt
@@ -0,0 +1,35 @@
+package generics
+
+fun genericsExample() {
+    val aquarium = Aquarium(TapWater())
+    addItemTo(aquarium)
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+open class WaterSupply(var needsProcessing: Boolean)
+
+class TapWater : WaterSupply(true) {
+    fun addChemicalCleaners() {
+        needsProcessing = false
+    }
+}
+
+class FishStoreWater : WaterSupply(false)
+
+class LakeWater : WaterSupply(true) {
+    fun filter() {
+        needsProcessing = false
+    }
+}
+
+class Aquarium<out T: WaterSupply>(val waterSupply: T) {
+    fun addWater() {
+        check(!waterSupply.needsProcessing) { "water supply needs processing first" }
+        println("adding water from $waterSupply")
+    }
+}
+
+fun main() {
+    genericsExample()
+}
---
+++ src/main/kotlin/hello_world.kt
@@ -0,0 +1,62 @@
+import java.util.*    // required import
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else  -> false
+    }
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "red worms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+}
